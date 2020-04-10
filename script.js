f1f2cb8cdf9b02ddaddead9ce76b8f58bac572a9
zrton1 <zrton1@student.monash.edu>
2020-04-10 18:01:16 +1000

    With changing sound bank
    added counter
    added sound bank change
    added new sound bank

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
 //This script links the 'flex''child class and links it to the audio
 const flex = document.querySelectorAll('.flex')
+let CurrentCount = 1;
+let flag = true;
 
 flex.forEach(flex => {
 	flex.addEventListener('click', ()=>playNote(flex))
 })
 
+
+//Check which sound bank to play
+function soundBank(CurrentCount){
+	if ((CurrentCount-1)%5 == 0){
+		flag = !flag;
+		console.log(flag);
+	}
+		if (flag == false){
+		document.getElementById("note1").dataset.note = "C"
+		document.getElementById("note2").dataset.note = "Db"
+		document.getElementById("note3").dataset.note = "D"
+		document.getElementById("note4").dataset.note = "Eb"
+		document.getElementById("note5").dataset.note = "E"
+		document.getElementById("note6").dataset.note = "F"
+		}
+		else{
+			document.getElementById("note1").dataset.note = "bubbles"
+			document.getElementById("note2").dataset.note = "clay"
+			document.getElementById("note3").dataset.note = "confetti"
+			document.getElementById("note4").dataset.note = "glimmer"
+			document.getElementById("note5").dataset.note = "moon"
+			document.getElementById("note6").dataset.note = "ufo"
+		}
+	}
+
+//Reads the note and plays sound
 function playNote(flex){
+	soundBank(CurrentCount);
 	const noteAudio = document.getElementById(flex.dataset.note)
 //setting current time to 0 means that the note can be re-played every time it's clicked
+	CurrentCount =  counter(CurrentCount);
 	noteAudio.currentTime = 0
 	noteAudio.play()
 	flex.classList.add('active')
 	noteAudio.addEventListener('ended' , ()=> {
 		flex.classList.remove('active')
 	})
+console.log(CurrentCount);
+}
+
+//Counter
+function counter(count){
+	count += 1;
+	return count;
 }
