bdeea5a0c68842910ad465c58c1fe201ed44d6d0
zrton1 <zrton1@student.monash.edu>
2020-04-10 19:40:42 +1000

    Added sound bank
    Added the 3rd soundbank

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 //This script links the 'flex''child class and links it to the audio
 const flex = document.querySelectorAll('.flex')
 let CurrentCount = 1;
-let flag = true;
+let bankNo = -1;
 
 flex.forEach(flex => {
 	flex.addEventListener('click', ()=>playNote(flex))
 })
 
 
+//When RL is implemented, bankNo will be an output of that algorithm
 //Check which sound bank to play
 function soundBank(CurrentCount){
 	if ((CurrentCount-1)%5 == 0){
-		flag = !flag;
-		console.log(flag);
+		console.log("first CurrentCount", CurrentCount)
+		bankNo += 1;
+		console.log("bankNo: ", bankNo);
 	}
-		if (flag == false){
+		if ((bankNo%3) == 0){
 		document.getElementById("note1").dataset.note = "C"
 		document.getElementById("note2").dataset.note = "Db"
 		document.getElementById("note3").dataset.note = "D"
@@ -22,13 +24,21 @@ function soundBank(CurrentCount){
 		document.getElementById("note5").dataset.note = "E"
 		document.getElementById("note6").dataset.note = "F"
 		}
-		else{
+		else if ((bankNo%3) == 1) {
 			document.getElementById("note1").dataset.note = "bubbles"
 			document.getElementById("note2").dataset.note = "clay"
 			document.getElementById("note3").dataset.note = "confetti"
 			document.getElementById("note4").dataset.note = "glimmer"
 			document.getElementById("note5").dataset.note = "moon"
 			document.getElementById("note6").dataset.note = "ufo"
+		}
+		else if ((bankNo%3) == 2){
+			document.getElementById("note1").dataset.note = "ding"
+			document.getElementById("note2").dataset.note = "ping"
+			document.getElementById("note3").dataset.note = "ring"
+			document.getElementById("note4").dataset.note = "ting"
+			document.getElementById("note5").dataset.note = "whistle"
+			document.getElementById("note6").dataset.note = "gong"
 		}
 	}
 
@@ -44,7 +54,7 @@ function playNote(flex){
 	noteAudio.addEventListener('ended' , ()=> {
 		flex.classList.remove('active')
 	})
-console.log(CurrentCount);
+console.log("CurrentCount:", CurrentCount);
 }
 
 //Counter
